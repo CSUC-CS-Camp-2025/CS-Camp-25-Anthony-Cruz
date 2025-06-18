8c100190e07e9c20e5cfa3df5b4d642bbde45397
Anthony Cruz <antho.cr04@gmail.com>
2025-06-18 20:22:56 +0000

    added header hover animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
+const logo = document.getElementById("topbar-item"); //init topbar item
+
+const logoKeyframes = [
+  { transform: "rotate(0) scale(1)" },
+  { transform: "rotate(360deg) scale(0)" },
+];
+
+const logoOptions = {
+  duration: 2000,
+  iterations: 1,
+};
+
+logo.addEventListener ('click', () => {
+    logo.animate(logoKeyframes, logoOptions);
 });
